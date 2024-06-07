f80b15eb2bbeff450db870f9bcd3724f790a9d84
993770025f <farnaz7380@gmail.com>
2024-06-07 19:32:57 +0330

    Add files via upload

---
+++ t2.py
@@ -0,0 +1,35 @@
+#متدهای غیر is:
+#center متد
+#طول رشتهرازیادکرده وآن را دروسط قرار میدهد
+txt="banana"
+print(txt.center(20,"o"))
+#count:تعداد دقعاتی که یک مقدار دریک رشته کرار شده را برمیگرداند
+A="i love apple,apple is my favorite fruit"
+print(A.count("apple"))
+#endswith:چک میکند آیارشته با کرکترخاصی تمام میشودیا خیر.
+B="welcome to my world"
+print(B.endswith("d"))
+#find:چک میکند که ایایک مقداردریک رشته وجودداردیاخیراگروجودداشت ایندکس ان رابرمیگردانداگرنداشت -1 برمیگرداند
+C="hello,welcome to my world"
+print(C.find("welcome"))
+#index:چک میکند که ایایک مقداردریک رشته وجودداردیاخیراگروجودداشت ایندکس ان رابرمیگردانداگرنداشت -1ارور میدهد
+D="hello,welcome to my world"
+print(D.index("hello"))
+#lower:یک رشته رابه حروف کوچک تبدیل میکند
+E="HELLO MY FRIENDS"
+print(E.lower())
+#replace:یک مقداررابایک مقداردیگرجابه جا میکند
+F="i like banana"
+print(F.replace("banana","apple"))
+#startswith:چک میکند که ایا یک رشته با کرکتر خاصی شروع میشودیاخیر
+G="hello,welcome to my world"
+print(G.startswith("hi"))
+#strip:فاصله های خالی ازیک رشته راحذف میکند
+H="   banana  "
+print(H.strip())
+#sowpcase:کرکترهای کوچک رابه بزرگ وکرکترهای بزرگ رابه کوچک تبدیل میکند
+I="hello my name is peter"
+print(I.swapcase())
+#upper:تبدیل کرکترهای کوچک به بزرگ
+J="Hello my friends"
+print(J.upper())
